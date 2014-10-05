8980387a1a6f2cc042169fc3049ae084ca46089f
alexm52 <alexmcdoom@hotmail.com>
2014-10-05 08:37:07 +0000

    Behavior appears identical to example. Commented code, including commenting out debug lines.

--- maze.js
+++ maze.js
@@ -1,31 +1,31 @@
-var bounds;
+var bounds;             //Var declarations
 var hit;
 
 window.onload = function(){
-    console.log("Window load");
-    //$("boundary1").onmouseover = borderCrossing;
-    hit = false;
+    //console.log("Window load");                           //DEBUGGING MSG
+    //$("boundary1").onmouseover = borderCrossing;                  //Old code (Exercise 1)
+    hit = false;                                                                                    //Vars initialized
     bounds = $$(".boundary");
-    console.log("retrieved bounds" + bounds.length);
-    for(var i=0; i<bounds.length; i++) {
+    //console.log("retrieved bounds" + bounds.length);      //DEBUGGING MSG
+    for(var i=0; i<bounds.length; i++) {                                                            //Set up border handler..
         bounds[i].onmouseover = borderCrossing;
-        console.log("Setting mouseover" + i);
+        //console.log("Setting mouseover" + i);             //DEBUGGING
     }
-    $("end").onmouseover = endHandler;
-    $("start").onmousedown = startHandler;
+    $("end").onmouseover = endHandler;                                                              //..end square handler..
+    $("start").onmousedown = startHandler;                                                          //..and start square handler
 };
 
-var borderCrossing = function(){
-    //$("boundary1").addClassName("youlose");
-    //bounds = $$(".boundary");
+var borderCrossing = function(){                                                                    //border handler defined
+    //$("boundary1").addClassName("youlose");                       //Old code (Exercise 1)
+    //bounds = $$(".boundary");                                     //Unused after Ex.1 ...
     hit = true;
     for(var i=0; i<bounds.length; i++) {
         bounds[i].addClassName("youlose");
-        console.log("add youlose" + i);
+        //console.log("add youlose" + i);                   //DEBUGGING
     }
 };
 
-var endHandler = function(){
+var endHandler = function(){                                                                        //end handler..
     if(hit === false){
         alert("You Win!");
     } else {
@@ -33,11 +33,11 @@ var endHandler = function(){
     }
 };
 
-var startHandler = function(){
-    console.log("Reset hit");
+var startHandler = function(){                                                                      //start handler..
+    //console.log("Reset hit");                             //DEBUGGING
     hit = false;
     for(var i=0; i<bounds.length; i++){
-        console.log("reset bounds" + i);
+        //console.log("reset bounds" + i);                  //DEBUGGING
         bounds[i].removeClassName("youlose");
     }
 };
