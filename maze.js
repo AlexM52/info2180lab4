fa004071dc06c0a28ac5debababeb6ec68056e53
alexm52 <alexmcdoom@hotmail.com>
2014-10-05 06:38:00 +0000

    Exercise 2 complete: all boundary div's change to red when any div border is crossed.

--- maze.js
+++ maze.js
@@ -1,7 +1,19 @@
 window.onload = function(){
-    $("boundary1").onmouseover = borderCrossing;
+    console.log("Window load");
+    //$("boundary1").onmouseover = borderCrossing;
+    var bounds = $$(".boundary");
+    console.log("retrieved bounds" + bounds.length);
+    for(var i=0; i<bounds.length; i++) {
+        bounds[i].onmouseover = borderCrossing;
+        console.log("Setting mouseover" + i);
+    }
 }
 
 var borderCrossing = function(){
-    $("boundary1").addClassName("youlose");
+    //$("boundary1").addClassName("youlose");
+    var bounds = $$(".boundary");
+    for(var i=0; i<bounds.length; i++) {
+        bounds[i].addClassName("youlose");
+        console.log("add youlose" + i);
+    }
 }
