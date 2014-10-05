b97765e11b9e876810a2a2ea4c419a66b9831097
alexm52 <alexmcdoom@hotmail.com>
2014-10-05 07:43:25 +0000

    Exercise 3 complete: Alert pops up on reaching end square without crossing any of the boundaries.

--- maze.js
+++ maze.js
@@ -1,21 +1,31 @@
 var bounds;
+var hit;
 
 window.onload = function(){
     console.log("Window load");
     //$("boundary1").onmouseover = borderCrossing;
+    hit = false;
     bounds = $$(".boundary");
     console.log("retrieved bounds" + bounds.length);
     for(var i=0; i<bounds.length; i++) {
         bounds[i].onmouseover = borderCrossing;
         console.log("Setting mouseover" + i);
     }
+    $("end").onmouseover = endHandler;
 }
 
 var borderCrossing = function(){
     //$("boundary1").addClassName("youlose");
     //bounds = $$(".boundary");
+    hit = true;
     for(var i=0; i<bounds.length; i++) {
         bounds[i].addClassName("youlose");
         console.log("add youlose" + i);
+    }
+}
+
+var endHandler = function(){
+    if(hit === false){
+        alert("You Win!");
     }
 }
